e6576a58588023fa351c45072ae986b4785e3b42
Stefan <stefan@jaggersystems.com>
2020-09-30 18:16:59 +0100

    Function build_candle_id build to return L or S candle details for 1 candle. Using local data list rather than CSV temporarily.

--- main.py
+++ main.py
@@ -1,44 +1,65 @@
-# Date[0], Close[1], Open[2], High[3], Low[4], Change %[5]
+# Date[0], Time[1], Open[2], High[3], Low[4], Close[5], Volume[6]
 
-import numpy as np
-import csv
+# import csv
+# with open('EURUSD.csv', "r", newline='') as csv_file:
+#     data = list(csv.reader(csv_file))
+# csv_file.close()
 
-with open('EURUSD.csv', "r", newline='') as csvfile:
-    data = list(csv.reader(csvfile))
-csvfile.close()
-
-# def calculate_c_percent(close, open, high, low):
+data = [[25,9,1.16677,1.16843,1.16114,1.16303,726665], [26,9,1.16245,1.16793,1.16144,1.16654,720030], [27,9,1.16654,1.17447,1.16608,1.17414,700881]]
 
 
-# print(data[1][1])
+def build_candle_id(close, open, high, low):
+    # print("Function Test:" + str(open) + "\n")
+    c_true_range = (high - low)
+    c_pips = int(c_true_range * 10000) # TODO: This is a hack fix to get to our 72 pips.
 
-c_close = float(data[1][1])
-c_open = float(data[1][2])
-c_high = float(data[1][3])
-c_low = float(data[1][4])
+    if close > open:
+        candle_direction = "L"
+        c_high_percent = ((high - close) / c_true_range) * 100
+        c_body_percent = ((close - open) / c_true_range) * 100
+        c_low_percent = 100 - c_high_percent - c_body_percent
+    else:
+        candle_direction = "S"
+        c_high_percent = ((high - open) / c_true_range) * 100
+        c_body_percent = ((open - close) / c_true_range) * 100
+        c_low_percent = 100 - c_high_percent - c_body_percent
+
+    candle_id = candle_direction + "-" + str(round(c_high_percent, 2)) + "-" + str(round(c_body_percent, 2)) + "-" + str(round(c_low_percent, 2)) + "-" + str(c_pips)
+    return candle_id
+
+# 0 = bear
+# 1 or 2 = bull
+day_selector = 1
+
+c_open = float(data[day_selector][2])
+c_high = float(data[day_selector][3])
+c_low = float(data[day_selector][4])
+c_close = float(data[day_selector][5])
 
 
-c_range = c_high - c_low
-
-c_high_percent = ((c_high - c_open)/c_range) * 100
-c_body_percent = ((c_open - c_close)/c_range) * 100
-c_low_percent = 100 - c_high_percent - c_body_percent
-
-green_candle = True
-
-if (c_open < c_close):
-    green_candle = True
-    c_price_movement = round((c_close - c_open), 4)
-else:
-    green_candle = False
-    c_price_movement = round((c_open - c_close), 4)
+# print("Open Ref: " + str(c_open) + "\n")
+print(build_candle_id(c_close, c_open, c_high, c_low))
 
 
-
-print("\nHigh%: " + str(round(c_high_percent) ))
-print("Body%: " + str(round(c_body_percent) ))
-print("Low%: " + str(round(c_low_percent) ))
-
-print("The Open is: " + str(c_open))
-print("The Close is: " + str(c_close))
-print("Pips: " + str(c_price_movement))
+# c_range = c_high - c_low
+#
+# c_high_percent = ((c_high - c_open)/c_range) * 100
+# c_body_percent = ((c_open - c_close)/c_range) * 100
+# c_low_percent = 100 - c_high_percent - c_body_percent
+#
+# green_candle = True
+#
+# if (c_open < c_close):
+#     green_candle = True
+#     c_price_movement = round((c_close - c_open), 4)
+# else:
+#     green_candle = False
+#     c_price_movement = round((c_open - c_close), 4)
+#
+# print("\nHigh%: " + str(round(c_high_percent) ))
+# print("Body%: " + str(round(c_body_percent) ))
+# print("Low%: " + str(round(c_low_percent) ))
+#
+# print("The Open is: " + str(c_open))
+# print("The Close is: " + str(c_close))
+# print("Pips: " + str(c_price_movement))
